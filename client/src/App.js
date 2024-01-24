89abd8c81ea5c53fca64ff1d4614a23c0d41ac66
Akash Agrawal <akashagrawal0358@gmail.com>
2024-01-24 23:18:32 +0530

    Register Page with validation almost completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom' ;
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Chat from './pages/Chat';
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/setAvatar" element={<SetAvatar />} /> */}
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </Router>
   )
 }
 
